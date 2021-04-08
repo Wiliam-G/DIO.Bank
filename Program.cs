861025cabc302b401cfc1b10a2089f6fe98e0844
unknown <wiliam.gomes99@gmail.com>
2021-04-07 23:17:19 -0300

    Corrigindo métodos duplicados

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace DIO.Bank
 {
     class Program
@@ -9,6 +10,7 @@ namespace DIO.Bank
         static List<Conta> listaContas = new List<Conta>();
         static void Main(string[] args)
         {
+            
             string opcaousuario = ObterOpcaoUsuario();
             while(opcaousuario.ToUpper() != "X")
             {
@@ -43,81 +45,6 @@ namespace DIO.Bank
             Console.WriteLine("Obrigado por utulizar nossos serviços.");
             Console.WriteLine();
 
-        }
-
-        private static void ListarContas()
-        {
-            Console.WriteLine("Listar contas");
-
-            if(listaContas.Count == 0)
-            {
-                Console.WriteLine("Não há contas cadastradas");
-                return;
-            }
-
-          for (int i = 0; i < listaContas.Count; i++)
-            {
-                Conta conta = listaContas[i];
-                Console.Write("#[0] - ", i);
-                Console.WriteLine(conta);
-            }
-        }
-        private static void InserirConta()
-        {
-            Console.WriteLine("Inserir nova conta");
-
-            Console.WriteLine("Digite 1 para conta física ou 2 para jurídica");
-            int entradaTipoConta = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o nome do cliente: ");
-            string entradaNome = Console.ReadLine();
-
-            Console.WriteLine("Digite o saldo inicial: ");
-            double entradaSaldo = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o crédito: ");
-            double entradaCredito = double.Parse(Console.ReadLine());
-
-            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
-                saldo: entradaSaldo,
-                credito: entradaCredito,
-                nome: entradaNome);
-
-            listaContas.Add(novaConta);
-        }
-        private static void Transferir()
-        {
-            Console.WriteLine("Digite o número da conta de origem: ");
-            int IndiceContaOrigem = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o número da conta de destino: ");
-            int indiceContaDestino = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o valor a ser transferido: ");
-            double valorTransferencia = double.Parse(Console.ReadLine());
-
-            listaContas[IndiceContaOrigem].Transferir(valorTransferencia, listaContas[indiceContaDestino]);
-        }
-        private static void Sacar()
-        {
-            Console.WriteLine("Digite o número da conta: ");
-            int IndiceConta = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o valor a ser sacado: ");
-            double valorSaque = double.Parse(Console.ReadLine());
-
-            listaContas[IndiceConta].Sacar(valorSaque);
-        }
-
-        private static void Depositar()
-        {
-            Console.WriteLine("Digite o número da conta: ");
-            int IndiceConta = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Digite o valor a ser depositado: ");
-            double valorDeposito = double.Parse(Console.ReadLine());
-
-            listaContas[IndiceConta].Depositar(valorDeposito);
         }
 
         private static void ListarContas()
